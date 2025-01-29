5fdefe57e90c0ebdedf6ce5d77e546022c9d0717
ellinor <ellinoora.riskala@metropolia.fi>
2025-01-29 16:34:49 +0200

    Initial commit

---
+++ src/MessageTypes.ts
@@ -0,0 +1,50 @@
+import { UserWithNoPassword } from './DBTypes';
+
+type MessageResponse = {
+  message: string;
+};
+
+type ErrorResponse = MessageResponse & {
+  stack?: string;
+};
+
+// for auth server
+type LoginResponse = MessageResponse & {
+  token: string;
+  message: string;
+  user: UserWithNoPassword;
+};
+
+type UserResponse = MessageResponse & {
+  user: UserWithNoPassword;
+};
+
+type UserDeleteResponse = MessageResponse & {
+  user: { user_id: number };
+};
+
+type AvailableResponse = Partial<MessageResponse> & {
+  available?: boolean;
+};
+
+type BooleanResponse = MessageResponse & {
+  success: boolean;
+};
+
+// for upload server
+type UploadResponse = MessageResponse & {
+  data: {
+    filename: string;
+  };
+};
+
+export type {
+  MessageResponse,
+  ErrorResponse,
+  LoginResponse,
+  UploadResponse,
+  UserResponse,
+  UserDeleteResponse,
+  AvailableResponse,
+  BooleanResponse,
+};
